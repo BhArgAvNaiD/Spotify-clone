dcc26725fdfd376c564f0f1e090269f03169d63b
BhArgAvNaiD <bhargav.naidu.mbn.18@gmail.com>
2023-02-10 18:19:41 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+console.log("Welcome to spotify");
+let songIndex=0;
+let audioElement = new Audio('manohara.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+let songs =
+[
+    {songName:"Thee Thalapathy" ,filePath:"1.mp3" , coverPath:"cover.png" },
+    {songName:"What the life" ,filePath:"2.mp3" , coverPath:"cover1.png" },
+    {songName:"Manohara" ,filePath:"3.mp3" , coverPath:"cover2.png" },
+    {songName:"ha ha" ,filePath:"4.mp3" , coverPath:"cover3.png" },
+]
+
+// songItems.forEach((element) => {
+//     element.getElementByTagName('img')[0].src= songs[i].coverPath;
+//     element.getElementByClassName('songName')[0].innerText = songs[i].songName;
+// })
+
+masterPlay.addEventListener('click', () => {
+    if(audioElement.paused || audioElement.currentTime<=0)
+    {
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
+    }
+});
+audioElement.addEventListener('timeupdate',() => {
+    console.log('timeupdate');
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+    console.log(progress);
+    myProgressBar.value=progress;
+});
+
+myProgressBar.addEventListener('change',()=>{
+    audioElement.currentTime = (myProgressBar.value*audioElement.duration)/100;
+})
